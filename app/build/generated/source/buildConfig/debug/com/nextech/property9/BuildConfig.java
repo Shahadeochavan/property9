123237116a0b53136b92f4e1dcf6e7998dfe589f
Shahadeo Chavan <shahadeochavan@gmail.com>
2016-08-27 18:52:52 +0530

    changes in homehomepropert9,and add new class homepropert9Adapetr

--- app/build/generated/source/buildConfig/debug/com/nextech/property9/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/nextech/property9/BuildConfig.java
@@ -5,7 +5,7 @@ package com.nextech.property9;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "info.androidhive.slidingmenu";
+  public static final String APPLICATION_ID = "com.nextech.property9";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- app/src/main/java/com/nextech/property9/HomeProperty9Fragment.java
+++ app/src/main/java/com/nextech/property9/HomeProperty9Fragment.java
@@ -1,29 +1,62 @@
 package com.nextech.property9;
+
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.ViewFlipper;
+
 public class HomeProperty9Fragment extends Fragment {
 	
 	public HomeProperty9Fragment(){}
     ViewFlipper viewFlipper;
     Animation Fade_in,Fade_out;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
         View v = inflater.inflate(R.layout.fragment_home_property9,
                 container, false);
         viewFlipper = (ViewFlipper) v.findViewById(R.id.bckgrndViewFlipper1);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mRecyclerView.setAdapter(mAdapter);
         viewFlipper.setInAnimation( Fade_in );
         viewFlipper.setOutAnimation( Fade_out );
         viewFlipper.setAutoStart( true );
         viewFlipper.setFlipInterval(5000);
         viewFlipper.startFlipping();
+        viewFlipper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                return false;
+            }
+        });
+        viewFlipper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.showNext();
+
+            }
+        });
+        viewFlipper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.showPrevious();
+            }
+        });
         return v;
     }
+
 
 
 }
